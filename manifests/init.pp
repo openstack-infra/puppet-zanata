f7fe088716aa53a348758c2ab045c5fe484d3862
Elizabeth K. Joseph <lyz@princessleia.com>
2015-06-17 14:21:23 -0700

    Rewrite download and unzip logic for modules
    Zanata module downloads were written so that unzip requires exec
    for the download. Swapping this so that download notifies unzip
    and unzip only runs when the downloaded file has been refreshed.
    Currently unzil is running with every puppet run.
    
    Change-Id: I4223d37f5dcff5c53bd7166291eb2f3915cd9546

--- manifests/init.pp
+++ manifests/init.pp
@@ -99,17 +99,19 @@ class zanata(
     timeout => 600,
     require => [
       Package['wget'],
+    notify  => Exec['unzip_hibernate'],
     ]
   }
 
   exec { 'unzip_hibernate':
-    command => "/usr/bin/unzip -o ${zanata_hibernate_file} -d /opt/wildfly/",
-    cwd     => '/home/wildfly',
-    user    => 'wildfly',
-    require => [
-      Exec['download_hibernate'],
+    command     => "/usr/bin/unzip -o ${zanata_hibernate_file} -d /opt/wildfly/",
+    cwd         => '/home/wildfly',
+    user        => 'wildfly',
+    require     => [
       Package['unzip'],
-    ]
+      File["/home/wildfly/${zanata_hibernate_file}"],
+    ],
+    refreshonly => true,
   }
 
   exec { 'download_mojarra':
@@ -120,17 +122,19 @@ class zanata(
     timeout => 600,
     require => [
       Package['wget'],
+    notify  => Exec['unzip_mojarra'],
     ]
   }
 
   exec { 'unzip_mojarra':
-    command => "/usr/bin/unzip -o ${zanata_mojarra_file} -d /opt/wildfly/",
-    cwd     => '/home/wildfly',
-    user    => 'wildfly',
-    require => [
-      Exec['download_mojarra'],
+    command     => "/usr/bin/unzip -o ${zanata_mojarra_file} -d /opt/wildfly/",
+    cwd         => '/home/wildfly',
+    user        => 'wildfly',
+    require     => [
       Package['unzip'],
-    ]
+      File["/home/wildfly/${zanata_mojarra_file}"],
+    ],
+    refreshonly => true,
   }
 
   file { '/opt/wildfly/standalone/deployments/mysql-connector-java.jar':
@@ -151,8 +155,6 @@ class zanata(
     require => [
                 Class['zanata::wildfly'],
                 File['/opt/wildfly/standalone/deployments/ROOT.war'],
-                Exec['unzip_mojarra'],
-                Exec['unzip_hibernate'],
                 ],
   }
 }
